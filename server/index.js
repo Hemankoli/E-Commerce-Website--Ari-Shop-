8814c12e66458143a131c3a8c0f1f2b362622458
Hemant  Koli <96255993+Hemankoli@users.noreply.github.com>
2025-08-10 16:24:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,9 +16,17 @@ const orderRoutesAdmin = require('./routes/Admin/orderRoute')
 
 
 
+const allowedOrigins = ["https://e-tail-ecommerce.vercel.app"];
+
 app.use(cors({
-    origin : ["https://e-tail-ecommerce.vercel.app"],
-    credentials : true
+  origin: function(origin, callback){
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      return callback(new Error('Not allowed by CORS'), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
 }));
 
 app.use(express.json());
