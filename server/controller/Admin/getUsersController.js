ac399faaa60221c2279b92213791c8e5299b7f24
Hemant  Koli <96255993+Hemankoli@users.noreply.github.com>
2025-08-10 16:01:48 +0530

    Update getUsersController.js

--- server/controller/Admin/getUsersController.js
+++ server/controller/Admin/getUsersController.js
@@ -1,7 +1,7 @@
 const pool = require("../../database/connection");
 
 exports.getAllUsers = (req, res) => {
-    pool.query("SELECT * FROM users;", (err, results) => {
+    pool.execute("SELECT * FROM users;", (err, results) => {
         if (err) {
             console.error("Error fetching users:", err);
             return res.status(500).json({ error: "Internal Server Error" });
