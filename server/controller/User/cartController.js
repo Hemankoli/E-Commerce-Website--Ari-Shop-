9bb1cce7ab845ba0b693a5b2072e54aa8bffab6c
Hemant  Koli <96255993+Hemankoli@users.noreply.github.com>
2025-08-10 15:14:26 +0530

    Update cartController.js

--- server/controller/User/cartController.js
+++ server/controller/User/cartController.js
@@ -3,11 +3,10 @@ const pool = require("../../database/connection");
 
 exports.getCartItems = async (req, res) => {
   const { userId } = req?.params;
-  console.log(userId)
    const query = `SELECT ci.cart_item_id, ci.product_id, ci.user_id, ci.quantity, p.productName, p.image, p.price 
                  FROM cart_items ci
                  JOIN products p ON ci.product_id = p.product_id
-                 WHERE ci.user_id = ? AND ci.status = 'active'`;
+                 WHERE ci.user_id = ?`;
   try {
     pool.query(query, [userId] , (error, result) => {
       if (error) {
@@ -30,23 +29,22 @@ exports.addItemToCart = (req, res) => {
   const { user_id, product_id, quantity } = req?.body;
 
   if(!user_id && !product_id){
-    console.log("missing fiels")
     return res.status(400).json({ error: 'Missing required fields' });
   }
-  pool.query('SELECT * FROM cart_items WHERE user_id = ? AND product_id = ?', [user_id, product_id], (err, result) => {
+  pool.execute('SELECT * FROM cart_items WHERE user_id = ? AND product_id = ?', [user_id, product_id], (err, result) => {
     if (err) {
       return res.status(500).json({ message: 'Database error' });
     }
     if (result.length > 0) {
       const newQuantity = result[0].quantity + quantity;
-      pool.query('UPDATE cart_items SET quantity = ? WHERE user_id = ? AND product_id = ?', [newQuantity, user_id, product_id], (updateErr) => {
+      pool.execute('UPDATE cart_items SET quantity = ? WHERE user_id = ? AND product_id = ?', [newQuantity, user_id, product_id], (updateErr) => {
         if (updateErr) {
           return res.status(500).json({ message: 'Error updating quantity' });
         }
         return res.status(200).json({ message: 'Quantity updated' });
       });
     } else {
-      pool.query('INSERT INTO cart_items (user_id, product_id, quantity) VALUES (?, ?, ?)', [user_id, product_id, quantity], (insertErr) => {
+      pool.execute('INSERT INTO cart_items (user_id, product_id, quantity) VALUES (?, ?, ?)', [user_id, product_id, quantity], (insertErr) => {
         if (insertErr) {
           return res.status(500).json({ message: 'Error adding product to cart' });
         }
