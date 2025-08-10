d58cd989fc5d682a63dbb2fb685df1a4cc56cce1
Hemant  Koli <96255993+Hemankoli@users.noreply.github.com>
2025-08-10 17:05:16 +0530

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 
 // connect MySql
-    const pool = mysql.createPool({
+    const pool = mysql.createConnection({
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
