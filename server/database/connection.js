42bc55844b886d0f2a52f200a6551342d98a9d28
HemanKoli <hemankoli1409@gmail.com>
2025-08-12 22:37:18 +0530

    refactor: Simplify MongoDB connection logic and remove unused options

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,18 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const mongoURL = process.env.MONGO_URL;
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Connected to MongoDB');
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('✅ Connected to MongoDB');
   } catch (err) {
     console.error('Not Connected');
-    process.exit(1);
   }
 };
 
