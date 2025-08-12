13b0edc66f9901af532e1d5108f1f960727d4b19
HemanKoli <hemankoli1409@gmail.com>
2025-08-12 22:44:05 +0530

    refactor: Remove unnecessary dependency from useEffect in AuthProvider and update import statement in Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-    import React, { useState, useEffect, useRef } from 'react';
+    import { useState, useEffect, useRef } from 'react';
     import { useNavigate } from 'react-router-dom';
     import logo from '../assets/logo.png';
     import { MdMenu, MdClose } from 'react-icons/md';
